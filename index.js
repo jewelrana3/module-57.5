b61f36dc9971ea06d8b5fc0da57ffda3ba8c95b4
jewelrana3 <juyelrana7752@gmail.com>
2023-04-12 23:16:58 +0600

    firt commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const phones = require('./phones.json')
+const app = express();
+const port=5000;
+
+app.get('/',(req,res)=>{
+    res.send('mama koi turrr baria ')
+});
+app.get('/phones',(req,res)=>{
+    res.send(phones)
+});
+
+app.get('/phones/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    console.log('id number ',id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
+})
+
+app.listen(port,()=>{
+    console.log(`ki baper tomer ${port}`)
+})
